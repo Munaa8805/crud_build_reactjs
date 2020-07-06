5fe32ce879cfa45171a1971943e4a1cafea07b43
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2020-07-06 09:31:33 +0800

    Updates

---
+++ precache-manifest.4c7d76586cf3e3deb80ac2d1bfa44aa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44af3b8a641ab1f29c92616717e88f22",
+    "url": "/Munaa8805/crud_build/index.html"
+  },
+  {
+    "revision": "3175dec7a83e51190a3b",
+    "url": "/Munaa8805/crud_build/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6e42aaff4addbb823305",
+    "url": "/Munaa8805/crud_build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3175dec7a83e51190a3b",
+    "url": "/Munaa8805/crud_build/static/js/2.c5b6b6e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Munaa8805/crud_build/static/js/2.c5b6b6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e42aaff4addbb823305",
+    "url": "/Munaa8805/crud_build/static/js/main.233d8201.chunk.js"
+  },
+  {
+    "revision": "a85e3bd9cbb8c1da75cb",
+    "url": "/Munaa8805/crud_build/static/js/runtime-main.1801da06.js"
+  }
+]);
